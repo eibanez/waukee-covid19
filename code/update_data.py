ccff81d2e2c70d749aae2a5b12bda62bba37aee6
Eduardo Ibanez <edu.ibanez@gmail.com>
2020-09-04 23:05:14 -0500

    Save building updates

--- code/update_data.py
+++ code/update_data.py
@@ -43,6 +43,10 @@ data = []
 prev_data = None
 curr_locs = {}
 
+data_buildings = []
+build_start = {}
+build_end = {}
+
 for f in files:
     time, students, staff, isolated, locations = extract_data(f)
     curr_data = (students, staff, isolated)
@@ -56,7 +60,41 @@ for f in files:
         }
         data.append(new_data)
         prev_data = curr_data
+    
+    for l in locations:
+        try:
+            build_start[l]
+        except KeyError:
+            build_start[l] = str(time)
+        build_end[l] = str(time)
+    
+    curr_buildings = {b: True for b in locations}
+    
+    all_builds = [k for k, _ in build_start.items()]
+    for b in all_builds:
+        if curr_buildings.get(b):
+            continue
+        
+        new_build = {
+            'location': b,
+            'start_utc': build_start[b], 
+            'end_utc': build_end[b], 
+        }
+        data_buildings.append(new_build)
+        build_start.pop(b)
+        build_end.pop(b)
 
+# Add any remaining dates
+for k, v in build_start.items():
+    new_build = {
+        'location': k,
+        'start_utc': build_start[k],
+        'end_utc': build_end[k], 
+    }
+    data_buildings.append(new_build)
 
 with open(os.path.join('docs', 'data.json'), 'w') as f:
     f.write(json.dumps(data, indent=2))
+
+with open(os.path.join('docs', 'buildings.json'), 'w') as f:
+    f.write(json.dumps(data_buildings, indent=2))
