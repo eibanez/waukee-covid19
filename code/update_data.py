4b2e4efa3ff25ebfc71606da68de5c2c59810567
Eduardo Ibanez <edu.ibanez@gmail.com>
2020-09-04 21:37:18 -0500

    Update update_data.py

--- code/update_data.py
+++ code/update_data.py
@@ -9,7 +9,18 @@ files.sort()
 def extract_data(fname):
     soup = BeautifulSoup(open(fname, 'r'), 'html.parser')
     table = soup.find('table')
-    print(table)
+    
+    for i, sect in enumerate(table.find_all('td')):
+        if i == 0:
+            students = int(sect.string)
+        elif i == 2:
+            staff = int(sect.string)
+        elif i == 4:
+            isolated = int(sect.string)
+        elif i == 6:
+            locations = int(sect.string)
+    
+    print(students, staff, isolated)
 
 for f in files[:1]:
     extract_data(os.path.join('pages', f))
