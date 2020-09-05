998f92050d60e634ace547b5a4cd9c3f6372a7b5
Eduardo Ibanez <edu.ibanez@gmail.com>
2020-09-04 20:53:27 -0500

    Update save_page.py

--- code/save_page.py
+++ code/save_page.py
@@ -1,12 +1,27 @@
 import datetime
-import os.path
+import os
 import urllib.request
 
+# Filename for the new file
 ts = datetime.datetime.utcnow()
 fname = str(ts).replace(':', '-').replace(' ', '_')[:19]
 fname = os.path.join('pages', 'page_' + fname + '.html')
-print(fname)
+print('New file:', fname)
+
+# Read the body of the last file
+files = os.listdir('pages')
+files.sort()
+last_file = os.path.join('pages', files[-1])
+print('Last file:', last_file)
+with open(last_file, 'rb') as f:
+    last_file_content = f.read()
 
 with urllib.request.urlopen('https://waukeeschools.org/rtl/covid-19-information-for-families/') as w:
-    with open(fname, 'wb') as f:
-        f.write(w.read())
+    new_file_content = w.read()
+    
+    if last_file_content == new_file_content:
+        print('SKIP: Contents are the same')
+    else:
+        print('Saving new file...')
+        with open(fname, 'wb') as f:
+           f.write(new_file_content)
